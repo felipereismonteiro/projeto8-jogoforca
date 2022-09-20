20f63e3d834de2aff8d92e3b57e12387ecd564e5
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-09-19 22:40:54 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom"
+
+export default function App() {
+    return(
+        <>
+            <img src="assets/forca0.png" alt="" />
+        </>
+    )
+}
+
+ReactDOM.render(App(), document.querySelector(".root"))
