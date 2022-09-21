6566e1bf853326e8eef07a4caafb189da433614b
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-09-21 16:05:40 -0300

    Layoout completo

---
+++ src/componentes/ImgForca.js
@@ -0,0 +1,9 @@
+import forca0 from "../assets/forca0.png"
+
+export default function ImgForca(props) {
+    
+    console.log(props)
+    return (
+        <img src={forca0} alt="forca" />  
+    )
+}
---
+++ src/componentes/Layout.js
@@ -0,0 +1,71 @@
+import ImgForca from "./ImgForca"
+
+import forca1 from "../assets/forca1.png"
+import forca2 from "../assets/forca2.png"
+import forca3 from "../assets/forca3.png"
+import forca4 from "../assets/forca4.png"
+import forca5 from "../assets/forca5.png"
+import forca6 from "../assets/forca6.png"
+
+
+export default function Layout() {
+    const alfabeto = [
+        "a", "b", "c", "d", "e", "f", "g", "h",
+        "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+        "v", "w", "x", "y", "z"]
+
+    const palavras = [
+        "abacate", "abacaxi", "abelha", "abanador", "antologia", "amor", "aba", "abraço", "ábaco", "abrigo", "abrir",
+        "banana", "boi", "batata", "bacalhau", "bexiga", "bowl", "batedeira", "bisturi", "barreira", "banco",
+        "caixa", "chantilly", "comércio", "cachorro", "cuidado", "caneta", "carinho", "cupuaçu", "cabra", "cesto",
+        "dados", "dizer", "danone", "dente", "diário", "diamante", "diafragma", "detonar", "dia", "dromedário",
+        "elefante", "esmeralda", "espátula", "estômago", "esfinge", "esfera", "encontro", "ema", "escola", "economia",
+        "formiga", "fama", "festa", "fiador", "ferver", "flauta", "fichário", "figo", "fiapo", "fotografia",
+        "goiaba", "gelo", "grito", "gamão", "guria", "goleiro", "golfinho", "golfe", "girar", "glúten",
+        "helicóptero", "harmonia", "haste", "hectare", "hábito", "hepatite", "hiena", "hemisfério", "hidrante",
+        "igreja", "ícone", "importante", "ímpar", "idoso", "irado", "identidade", "idioma", "idade", "idiota",
+        "jantar", "jumento", "jambú", "jibóia", "jararaca", "janela", "jerimum", "jaula", "jabuti", "jaleco",
+        "laranja", "lua", "leão", "limão", "larápio", "luz", "lindo", "lacraia", "lactose", "laço", "lacrar",
+        "mamão", "manga", "morango", "mico", "matar", "mingau", "moqueca", "macacão", "mocassin", "maçaneta",
+        "nectarina", "nada", "navio", "namorado", "ninja", "natal", "narciso", "narina", "nádega", "nabo",
+        "ovo", "ostra", "obstetra", "oblíquo", "orangotango", "olhar", "óculos", "ortodoxo", "ouro", "ornamento",
+        "pato", "polvo", "povoar", "pólvora", "palhaço", "paróqia", "pano", "princesa", "pizza", "patroa",
+        "queijo", "quitanda", "quinta", "quantia", "quarentena", "quadrilha", "quaresma", "quartzo", "quebrar", "quarteirão",
+        "risada", "rio", "remar", "rato", "racional", "rainha", "radioatividade", "raiz", "raiva", "rachadura",
+        "salada", "salamandra", "sacola", "siri", "sábado", "safanão", "sabre", "sucarose", "sabedoria", "sacerdote",
+        "tatu", "tabacaria", "taberneiro", "tábua", "torrada", "três", "terço", "tamanho", "tatuagem", "trem",
+        "uva", "uísque", "união", "universo", "unanimidade", "ubuntu", "universidade", "urso", "uivar", "unir",
+        "vela", "valeta", "vacilo", "valor", "vagem", "vadiagem", "vaca", "valentia", "vidro", "valsa",
+        "xícara", "xadrez", "xilofone", "xarope", "xenofobia", "xereta", "xerife", "xaveco", "xixi", "xale",
+        "zebra", "zagueiro", "zero", "zoeira", "zodíaco", "zangão", "zepelim", "zinco", "zoológico", "zumbido"
+    ]
+
+    return (
+        <>
+            <div className="container">
+                <div className="forca">
+                        <ImgForca contadorErro="" />
+                    <div className="escolherPalavra">
+                        <button className="escolherPalavraP">
+                            Escolher palavra
+                        </button>
+
+                        <div className="palavra">
+                            <h1>_ _ _ _ _ _ _ _ _ _</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="alfabeto">
+                    {alfabeto.map((l) => <button className="alfabetoButtonEnable">{l}</button> )}
+                </div>
+
+                <div className="jaSei">
+                    <p>Ja sei a palavra!</p>
+                    <input type="text" />
+                    <button className="chutar">chutar</button>
+                </div>
+            </div>
+        </>
+    )
+}
